b001c37e6419bbc233a3cfe658c6158a57571133
Jacob Preciado <jacobpreciado@gmail.com>
2025-03-17 22:49:20 -0600

    W03 Learning Activities

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -19,9 +21,9 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index Route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
