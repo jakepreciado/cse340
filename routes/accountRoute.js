d18c0c8b75b0b3de38ec884f8c219e72480a05da
Jake Preciado <jacobpreciado@gmail.com>
2025-03-24 14:35:37 -0600

    Learning Activities: Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,12 +2,19 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 
 module.exports = router;
