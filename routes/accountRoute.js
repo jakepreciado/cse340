82a670aef0b6616da791e8376dcc4f4539cdb24c
Jacob Preciado <jacobpreciado@gmail.com>
2025-03-25 00:19:18 -0600

    Team Activity 4

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const router = new express.Router() 
+const router = new express.Router()
 const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
 const regValidate = require('../utilities/account-validation')
@@ -10,11 +10,18 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 // Process the registration data
 router.post(
-    "/register",
-    regValidate.registrationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
-  )
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 module.exports = router;
